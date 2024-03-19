6f3114e0e9bfcbcc439e8a659844cd53134f684d
VadimBarinov <barinovvadim87@gmail.com>
2024-03-20 01:33:01 +0400

    added search elements

---
+++ js/catalogFilter.js
@@ -0,0 +1,33 @@
+const searchForm = document.forms["searchForm"]; // считываем форму
+
+const allCards = document.querySelectorAll(".my-card-col");
+
+searchForm.addEventListener("input", inputHandler);
+
+addEventListener("keydown", (e) => {
+  // не срабатывает enter
+  if (e.key === "Enter") e.preventDefault();
+});
+
+function inputHandler({ target }) {
+  inputCheck(target);
+}
+
+function inputCheck(el) {
+  const inputValue = el.value;
+  console.log(inputValue);
+
+  allCards.forEach((card) => {
+    const thisCardTitle = card.querySelector(".card-title").textContent;
+    const thisCardText = card.querySelector(".card-text").textContent;
+
+    if (
+      thisCardTitle.toLowerCase().indexOf(inputValue.toLowerCase()) != -1 ||
+      thisCardText.toLowerCase().indexOf(inputValue.toLowerCase()) != -1
+    ) {
+      card.classList.remove("hide-card");
+    } else {
+      card.classList.add("hide-card");
+    }
+  });
+}
